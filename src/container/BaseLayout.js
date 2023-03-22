b602ef0f087aca0edddb7c455eb5e8faf38c2ea7
Mariana Orrego <mariana.orregof@gmail.com>
2023-03-22 07:04:16 -0500

    feat: maquinas virtuales configuración

---
+++ src/container/BaseLayout.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useMachine } from '@xstate/react';
+
+import bookingMachine from '../machines/bookingMachine';
+import {Nav} from '../components/Nav'
+import { StepsLayout } from './StepsLayout';
+
+import './BaseLayout.css';
+
+export const BaseLayout = () => {
+
+    const [state, send] = useMachine(bookingMachine);
+
+    console.log('Maquinia inicializada',state.value, state.context)
+    return (
+        <div className='BaseLayout'>
+           <Nav state={state} send={send}/>
+           <StepsLayout state={state} send={send}/>
+        </div>
+    )
+}
---
+++ src/machines/bookingMachine.js
@@ -0,0 +1,102 @@
+import { assign, createMachine } from 'xstate';
+import { fetchCountries } from '../utils/api';
+
+const fillCountries = {
+  initial: "loading",
+  states:{
+    loading: {
+      //consumo de servicios
+      invoke: {
+        id: 'getCountries',
+        src: () => fetchCountries,
+        onDone: {
+          target: 'success',
+          actions: assign({
+            countries: (context, event) => event.data,
+          })
+        },
+        onError:{
+          target: 'failure',
+          actions: assign({
+            error: 'Fallo el request',
+          })
+        }
+      }
+    },
+    success: {},
+    failure:{
+      on:{
+        RETRY: {
+          target: "loading"
+        }
+      }
+    }
+  }
+}
+
+const bookingMachine = createMachine({
+  id: "Comprar tickets de vuelo",
+  //estado inicial
+  initial: "initial",
+  //contexto
+  context:{
+    passengers:[],
+    selectedCountry: '',
+    countries: [],
+    error: '',
+  },
+  //estados
+  states: {
+    initial: {
+      //eventos que generan transiciones
+      on: {
+        START: {
+          target: "search"
+        },
+      }
+    },
+    search: {
+      on: {
+        CONTINUE: {
+          target: 'passengers',
+          actions: assign({
+            selectedCountry: (context, event) => event.selectedCountry
+          })
+        },
+        CANCEL: "initial"
+      },
+      ...fillCountries,
+    },
+    passengers: {
+      on: {
+        DONE: "tickets",
+        CANCEL: {
+          target: 'initial',
+          actions: 'cleanContext',
+        },
+        ADD: {
+          target: 'passengers',
+          actions: assign(
+            (context, event) => context.passengers.push(event.newPassenger)
+          )
+        }
+      }
+    },
+    tickets: {
+      on: {
+        FINISH: "initial"
+      }
+    }
+  }
+},
+  {
+    actions: {
+      cleanContext: assign({
+        selectedCountry: '',
+        passengers: []
+      })
+    }
+  }
+);
+
+export default bookingMachine;
